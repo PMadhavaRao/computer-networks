a513cb05ce6249a1b311a84fdc94bb32ff779739
PMadhavaRao <139677650+PMadhavaRao@users.noreply.github.com>
2024-03-04 11:30:06 +0530

    Add files via upload

--- crc_polynomials.py
+++ crc_polynomials.py
@@ -1,43 +1,50 @@
-def xor(a, b):
-    result = []
-    for i in range(1, len(b)):
-        if a[i] == b[i]:
-            result.append('0')
-        else:
-            result.append('1')
-
-    return ''.join(result)
-
-def mod2div(dividend, divisor):
-    pick = len(divisor)
-    tmp = dividend[0: pick]
-    while pick < len(dividend):
-        if tmp[0] == '1':
-            tmp = xor(divisor, tmp) + dividend[pick]
-        else:
-            tmp = xor('0' * pick, tmp) + dividend[pick]
-        pick += 1
-    if tmp[0] == '1':
-        tmp = xor(divisor, tmp)
+def getRealData(rn,k):
+    n=rn+"0"*(len(k)-1)
+    ws=len(k)
+    ans=n[:ws]
+    for i in range(ws,len(n)):
+        add=n[i]
+        if len(ans)==ws:
+            temp=ans
+            ans=""
+            for i in range(ws):
+              res=str(int(k[i])^int(temp[i]))
+              ans+=res
+            ans=str(int(ans))
+        ans=ans+add
+    ans=str(int(ans)^int(k))
+    if ans!="0":
+        op=rn+"0"*(ws-len(ans)-1)+ans
     else:
-        tmp = xor('0' * pick, tmp)
-    checkword = tmp
-    return checkword
+        op=rn+"0"*(ws-1)
+    return op
+def receiver(rn,k):
+    n=rn
+    ws=len(k)
+    ans=n[:ws]
+    for i in range(ws,len(n)):
+        add=n[i]
+        if len(ans)==ws:
+            temp=ans
+            ans=""
+            for i in range(ws):
+              res=str(int(k[i])^int(temp[i]))
+              ans+=res
+            ans=str(int(ans))
+        ans=ans+add
+    ans=str(int(ans)^int(k))
 
-def encodeData(data, key):
-    l_key = len(key)
-    appended_data = data + '0' * (l_key - 1)
-    remainder = mod2div(appended_data, key)
-    codeword = data + remainder
-    print("Remainder: ", remainder)
-    print("Encoded Data (Data + Remainder): ", codeword)
+    if ans!="0":
+        print("Correct Data not received")
+    else:
+        print("Correct Data received")
+        
+n=input("Enter the Data : ")
+k=input("Enter the Key : ")
 
-data = "100100"
-key = "1101"
-encodeData(data, key)
+ans=getRealData(n,k)                                                                     
 
-
-'''
-output
-Remainder:  001
-Encoded Data (Data + Remainder):  100100001 '''
+print("Real Data is ",ans)
+check=input("Enter Data : ")
+key=input("Enter Key : ")
+receiver(check,k)
